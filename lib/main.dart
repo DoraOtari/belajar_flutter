a8119e968d3035e5054d306cad8da6263c9d0844
Hera Setiawan <doraotari@gmail.com>
2024-02-05 11:11:54 +0700

    second push

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 void main() {
   runApp(const MaterialApp(
@@ -69,12 +72,35 @@ class HomePage extends StatelessWidget {
 }
 
 Widget badanTabBar() {
-  return const TabBarView(
-    children: <Widget> [
-      Text("komunitas"),
-      Text("chat"),
-      Text("pembaruan"),
-      Text("panggilan"),
+  var waktu = DateFormat.Hm().format(DateTime.now());
+  List<Map<String, String>> isiItems = [
+   { 'Hera': 'pinjam dulu seratus',
+    'Heru' : 'main yuk',
+    'budi',
+    'Agus',
+    'satria',
+    'agung',
+    'budiman',}
+  ];
+  var items = List.generate(isiItems.length, (index) => isiItems[index]);
+  print(items[3]);
+  return TabBarView(
+    children: <Widget>[
+      const Text("komunitas"),
+      ListView.builder(
+        itemCount: items.length,
+        itemBuilder: (context, index) {
+          return ListTile(
+            leading: const CircleAvatar(
+                backgroundImage: NetworkImage("https://picsum.photos/200")),
+            title: const Text("Hera Setiawan"),
+            subtitle: const Text("pinjam dulu seratus besok saya ganti"),
+            trailing: Text(waktu.toString()),
+          );
+        },
+      ),
+      const Text("pembaruan"),
+      const Text("panggilan"),
     ],
   );
 }
